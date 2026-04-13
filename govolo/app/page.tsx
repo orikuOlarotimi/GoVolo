5a852fc7f19c66b1f5624ed7b6da632799cd7190
orikuOlarotimi <rotimipraise07@gmail.com>
2026-04-13 19:39:08 +0100

    merged both working branches

--- govolo/app/page.tsx
+++ govolo/app/page.tsx
@@ -1,13 +1,17 @@
 import MainHero from "@/components/homepages/MainHero";
 import Destinations from "@/components/homepages/Destinations";
 import AdventureCard from "@/components/homepages/AdventureCard";
+import Features from "@/components/homepages/Features";
+import Testimonials from "@/components/homepages/Testimonials";
 
 export default function Home() {
   return (
-    <div className="">
+    <div className="flex flex-col min-h-screen">
       <MainHero />
       <Destinations />
-      <AdventureCard/>
+      <AdventureCard />
+      <Features />
+      <Testimonials />
     </div>
   );
 }
