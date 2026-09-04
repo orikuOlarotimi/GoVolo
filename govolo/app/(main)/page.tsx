3a4318101d5a6acd62aef5c273a554ab04c5ddb0
orikuOlarotimi <rotimipraise07@gmail.com>
2026-09-04 20:48:07 +0100

    started integration with the backend resources

--- govolo/app/(main)/page.tsx
+++ govolo/app/(main)/page.tsx
@@ -8,15 +8,52 @@ import CTA from "@/components/homepages/CTA";
 import Contact from "@/components/homepages/Contact";
 import Newsletter from "@/components/homepages/Newsletter";
 
-export default function Home() {
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
+
+async function safeJson(res: PromiseSettledResult<Response>) {
+  if (res.status === "fulfilled" && res.value.ok) {
+    return await res.value.json();
+  }
+  return [];
+}
+
+
+
+export default async function Home() {
+  
+  const [destinationsRes, bookingsRes, testimonialsRes, blogsRes] =
+    await Promise.allSettled([
+      fetch(`${API_URL}/api/destinations/top-destinations?limit=3`, {
+        next: { revalidate: 60 },
+      }),
+      fetch(`${API_URL}/api/booking?limit=3`, {
+        next: { revalidate: 60 },
+      }),
+      fetch(`${API_URL}/api/testimonial?limit=4`, {
+        next: { revalidate: 60 },
+      }),
+      fetch(`${API_URL}/api/blogs?limit=3`, {
+        next: { revalidate: 60 },
+      }),
+    ]);
+
+  const [destinations, bookings, testimonials, blogs] = await Promise.all([
+    safeJson(destinationsRes),
+    safeJson(bookingsRes),
+    safeJson(testimonialsRes),
+    safeJson(blogsRes),
+  ]);
+
+
+
   return (
     <div className="flex flex-col min-h-screen">
       <MainHero />
-      <Destinations />
-      <AdventureCard />
+      <Destinations data={destinations} />
+      <AdventureCard data={bookings} />
       <Features />
-      <Testimonials />
-      <Blog />
+      <Testimonials data={testimonials} />
+      <Blog data={blogs} />
       <CTA />
       <Contact />
       <Newsletter />
